a8d3490797bce242f97adedd1620bc53f8867bd1
github_Irdannn <idanirdan001@gmail.com>
2022-06-28 20:19:05 +0700

    Added mad-lib replacement variable to story

---
+++ app.js
@@ -0,0 +1,41 @@
+"Use Strict"
+
+let userInputs = [];
+
+let originalStory = `<h2>Rapunzel grew into the most beautiful child beneath the ${userInputs[0]}.  When she was twelve years old, ${userInputs[1]} shut her into a tower, which lay in a forest.  The tower had no ${userInputs[2]} or ${userInputs[3]}, but only a little ${userInputs[4]} at the very top.  When the witch wanted to go in, she stood beneath the ${userInputs[5]} and cried,</h2>
+  <h2>"Rapunzel, Rapunzel,</h2>
+  <h2>Let down your ${userInputs[6]}."</h2>
+  <h2>Rapunzel had magnificent long ${userInputs[7]}, fine as spun gold, and when she heard the voice of the witch she wound her braids round one of the hooks of the window, and then the hair fell down the side of the tower and the witch climbed up by it.</h2>
+  <h2>After a year or two, it came to pass that the ${userInputs[8]} rode through the forest and went by the tower.  He heard a song which was so lovely that he stood still and listened.  This was Rapunzel who in her loneliness passed her time singing.  The Prince wanted to climb up to her, and looked for the door of the tower, but none was to be found.  He rode home, but the singing had so deeply touched his heart, that every day he went out into the forest and listened to it.</h2>
+  <h2>Once when he was standing behind a ${userInputs[9]} listening to Rapunzel's song, he saw the witch come and heard how she cried,</h2>
+  <h2>"Rapunzel, Rapunzel,</h2>
+  <h2>Let down your ${userInputs[10]}."</h2>
+  <h2>Then Rapunzel let down the braids of her ${userInputs[11]}, and the witch climbed up to her.</h2> 
+  <h2>"If that is the ${userInputs[12]} by which one mounts, I will for once try my fortune," thought the Prince and the next day when it began to grow dark, he went to the tower and ${userInputs[13]},</h2>
+  <h2>"Rapunzel, Rapunzel,</h2>
+  <h2>Let down your ${userInputs[14]}."</h2>
+  <h2>Immediately the ${userInputs[15]} fell down and the Prince climbed up.</h2>`;
+  console.log(originalStory);
+
+
+
+/*Rapunzel grew into the most beautiful child beneath the sun.  When she was twelve years old, the witch shut her into a tower, which lay in a forest.  The tower had no stairs or doors, but only a little window at the very top.  When the witch wanted to go in, she stood beneath the window and cried,
+
+"Rapunzel, Rapunzel,
+Let down your hair."
+
+Rapunzel had magnificent long hair, fine as spun gold, and when she heard the voice of the witch she wound her braids round one of the hooks of the window, and then the hair fell down the side of the tower and the witch climbed up by it.
+After a year or two, it came to pass that the Prince rode through the forest and went by the tower.  He heard a song which was so lovely that he stood still and listened.  This was Rapunzel who in her loneliness passed her time singing.  The Prince wanted to climb up to her, and looked for the door of the tower, but none was to be found.  He rode home, but the singing had so deeply touched his heart, that every day he went out into the forest and listened to it. 
+
+Once when he was standing behind a tree listening to Rapunzel's song, he saw the witch come and heard how she cried,
+
+"Rapunzel, Rapunzel,
+Let down your hair."
+
+Then Rapunzel let down the braids of her hair, and the witch climbed up to her. 
+"If that is the ladder by which one mounts, I will for once try my fortune," thought the Prince and the next day when it began to grow dark, he went to the tower and cried,
+
+"Rapunzel, Rapunzel,
+Let down your hair."
+
+Immediately the hair fell down and the Prince climbed up.*/
