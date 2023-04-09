c0d1656ad2d3425a8f24170f0d5e7e183a941aa1
irdannn <hallo@akuirdan.com>
2023-04-09 15:19:05 +0700

    update

--- app.js
+++ app.js
@@ -54,24 +54,3 @@ alert(
 
 console.log(originalStory);
 document.write(originalStory);
-
-/*Rapunzel grew into the most beautiful child beneath the sun.  When she was twelve years old, the witch shut her into a tower, which lay in a forest.  The tower had no stairs or doors, but only a little window at the very top.  When the witch wanted to go in, she stood beneath the window and cried,
-
-"Rapunzel, Rapunzel,
-Let down your hair."
-
-Rapunzel had magnificent long hair, fine as spun gold, and when she heard the voice of the witch she wound her braids round one of the hooks of the window, and then the hair fell down the side of the tower and the witch climbed up by it.
-After a year or two, it came to pass that the Prince rode through the forest and went by the tower.  He heard a song which was so lovely that he stood still and listened.  This was Rapunzel who in her loneliness passed her time singing.  The Prince wanted to climb up to her, and looked for the door of the tower, but none was to be found.  He rode home, but the singing had so deeply touched his heart, that every day he went out into the forest and listened to it. 
-
-Once when he was standing behind a tree listening to Rapunzel's song, he saw the witch come and heard how she cried,
-
-"Rapunzel, Rapunzel,
-Let down your hair."
-
-Then Rapunzel let down the braids of her hair, and the witch climbed up to her. 
-"If that is the ladder by which one mounts, I will for once try my fortune," thought the Prince and the next day when it began to grow dark, he went to the tower and cried,
-
-"Rapunzel, Rapunzel,
-Let down your hair."
-
-Immediately the hair fell down and the Prince climbed up.*/
